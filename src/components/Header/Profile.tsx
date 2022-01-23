25ecb92693a8b73586a3c66b5415a684bd65fa86
Nilton Izaguirres <niltonizaguirres@gmail.com>
2022-01-23 19:14:56 -0300

    feat: Responsive header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right" >
-        <Text>Nilton Izaguirres</Text>
-        <Text color="gray.300" fontSize="small" >
-          niltonizaguirres2003@gmail.com
-        </Text>
-      </Box>
+      {showProfileData &&
+        <Box mr="4" textAlign="right" >
+          <Text>Nilton Izaguirres</Text>
+          <Text color="gray.300" fontSize="small" >
+            niltonizaguirres2003@gmail.com
+          </Text>
+        </Box>
+      }
 
       <Avatar size="md" name="Nilton Izaguirres" src="https://www.github.com/niltonizaguirres.png" />
     </Flex>
