8c96fda2f0fa7551236466c9d4757e27e52dac79
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2022-07-15 14:11:44 -0300

    feat: added react-query configuration

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,38 +1,54 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
-import Link from "next/link";
-import { useEffect } from "react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import Pagination from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useBreakpointValue,
+} from '@chakra-ui/react';
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { RiAddLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
+
+import { Header } from '../../components/Header';
+import Pagination from '../../components/Pagination';
+import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = response.json();
+
+    return data;
+  });
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
-  useEffect(()=> {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, [])
+  useEffect(() => {}, []);
 
   return (
     <Box>
       <Header />
-      <Flex
-        w="100%"
-        my="6"
-        maxWidth={1480}
-        mx="auto"
-        px="6"
-      >
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
         <Box flex="1" borderRadius="8" bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
 
             <Link href="/users/create" passHref>
               <Button
@@ -47,78 +63,93 @@ export default function UserList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8" >
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="8"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Nilton Izaguirres</Text>
-                    <Text fontSize="sm" color="gray.300">niltonizaguirres2003@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion  && <Td>10 de Janeiro, 2021</Td>}
-                
-              </Tr>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <FLex justify="center">
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </FLex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="8"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Nilton Izaguirres</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          niltonizaguirres2003@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Nilton Izaguirres</Text>
-                    <Text fontSize="sm" color="gray.300">niltonizaguirres2003@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion  && <Td>10 de Janeiro, 2021</Td>}
-                
-              </Tr>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Nilton Izaguirres</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          niltonizaguirres2003@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Nilton Izaguirres</Text>
-                    <Text fontSize="sm" color="gray.300">niltonizaguirres2003@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion  && <Td>10 de Janeiro, 2021</Td>}
-                
-              </Tr>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Nilton Izaguirres</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          niltonizaguirres2003@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
+                  </Tr>
 
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Nilton Izaguirres</Text>
-                    <Text fontSize="sm" color="gray.300">niltonizaguirres2003@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion  && <Td>10 de Janeiro, 2021</Td>}
-                
-              </Tr>
-            </Tbody>
-          </Table>
+                  <Tr>
+                    <Td px={['4', '4', '6']}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Nilton Izaguirres</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          niltonizaguirres2003@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
-
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
