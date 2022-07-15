e72395442b7dc42f226b86c26e69af55dff32d49
Nilton Izaguirres <niltonizaguirres2003@gmail.com>
2022-07-15 14:32:10 -0300

    feat: list users with react-query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -27,16 +27,27 @@ import { Sidebar } from '../../components/Sidebar';
 export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
-    const data = response.json();
+    const data = await response.json();
 
-    return data;
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+      };
+    });
+
+    return users;
   });
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
-
-  useEffect(() => {}, []);
 
   return (
     <Box>
@@ -68,9 +79,9 @@ export default function UserList() {
               <Spinner />
             </Flex>
           ) : error ? (
-            <FLex justify="center">
+            <Flex justify="center">
               <Text>Falha ao obter dados dos usuários.</Text>
-            </FLex>
+            </Flex>
           ) : (
             <>
               <Table colorScheme="whiteAlpha">
@@ -85,65 +96,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Nilton Izaguirres</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          niltonizaguirres2003@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Nilton Izaguirres</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          niltonizaguirres2003@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Nilton Izaguirres</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          niltonizaguirres2003@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
-                  </Tr>
-
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Nilton Izaguirres</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          niltonizaguirres2003@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>10 de Janeiro, 2021</Td>}
-                  </Tr>
+                  {data.map((user) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
 
